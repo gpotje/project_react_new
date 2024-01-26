bd5d0f6261ffc39a98b7b573d9e7a0cbf7807ff9
Gabriel Potje de Souza <gabrielps@apoioprodesp.sp.gov.br>
2024-01-26 08:45:09 -0300

    fire base

--- App.js
+++ App.js
@@ -3,30 +3,45 @@ criar com expo
 npx create-expo expo-app
 npx expo start
 yarn add expo
-
+npx expo install firebase
+https://docs.nativebase.io/input
+npm install native-base
+import { NativeBaseProvider, Box } from "native-base";
  */
 
-
 import React, { Component } from 'react';
+import { NativeBaseProvider, Box,Input,Stack,extendTheme } from "native-base";
 import { 
-  Text, 
-  View, 
+ View, 
+ Text,
   StyleSheet,
 } from 'react-native';
+import firebase from './src/fireBaseConnection';
 
 
 class App extends Component{
 
   render(){
     return(
-      <View style={styles.container}>
-
-        <Text>Ola mundo</Text>
-
+      
+      <View  style={styles.container}>
+          <NativeBaseProvider>
+              <Stack direction="column" mb="" mt="1.5" space={3}>
+                  <Input size="2xl" placeholder="2xl Input" />
+              </Stack>
+              <Box>
+                óla mundo
+              </Box>
+              
+          </NativeBaseProvider>
+     
 
       </View>
-
+       
+     
     );
+        
+        
   }
 }
 
@@ -34,9 +49,9 @@ const styles = StyleSheet.create({
   
     container:{
       flex:1,
-      alignItems:'center',
-      justifyContent:'center',
-      backgroundColor: '#00aeef'
+      justifyContent:"center",
+      marginTop:40
+
     },
   
 });
---
+++ src/firebaseConnection.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCUbE348so9owd0nTokggpUm8BMxX9Idbk",
+    authDomain: "aulafirebase-3c7b2.firebaseapp.com",
+    projectId: "aulafirebase-3c7b2",
+    storageBucket: "aulafirebase-3c7b2.appspot.com",
+    messagingSenderId: "41780819521",
+    appId: "1:41780819521:web:8c5d209721ed92ead2f8c0",
+    measurementId: "G-DV96RZ4YFJ"
+  };
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export default firebase;
