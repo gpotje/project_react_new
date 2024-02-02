36c950473a6a32656cc4349dfc5479c64ab0b2bf
Gabriel Potje de Souza <gabrielps@apoioprodesp.sp.gov.br>
2024-02-02 16:19:02 -0300

    carinho ok

--- App.js
+++ App.js
@@ -3,14 +3,17 @@ import { NavigationContainer } from '@react-navigation/native';
 import { NativeBaseProvider, Box } from "native-base";
 
 import Routes from './src/routes/index';
+import CartProvider from './src/context/CartContext'
 
 
 export default function App(){
   return (
     <NativeBaseProvider>
       <NavigationContainer>
+        <CartProvider>
         <StatusBar backgroundColor="#Fafafa" />
         <Routes />
+        </CartProvider>
       </NavigationContainer>
     </NativeBaseProvider>
   );
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import {  Box, Pressable, FlatList, Text  } from "native-base";
 import { Feather } from '@expo/vector-icons'
 import Product from '../../component/product';
+import { useNavigation } from '@react-navigation/native';
+import { CartContext } from '../../context/CartContext';
 
 export default function Home(){
 
+  const { cart } = useContext(CartContext)
+
+  const navigation = useNavigation();
+  
   const [products, setProducts] = useState([
     {
       id: '1',
@@ -37,11 +43,12 @@ export default function Home(){
     <Box flex={1} safeArea backgroundColor="#fafafa" paddingY={14}> 
       <Box flexDirection="row" justifyContent="space-between" alignItems="center" paddingX={5}>
       <Text fontSize={24}>Lista de produtos</Text>
-      <Pressable alignItems="center" >
+
+      <Pressable alignItems="center"  onPress={ () => navigation.navigate("Cart")}>
           <Box alignItems="center" backgroundColor="#FC0303" justifyContent="center"
           w={5} h={5} borderRadius={10} position="absolute" zIndex={99}
           bottom={-2} left={-4} fontSize={11}
-          >99</Box>
+          >{cart?.length}</Box>
           <Box>
             <Feather 
             name='shopping-cart'
